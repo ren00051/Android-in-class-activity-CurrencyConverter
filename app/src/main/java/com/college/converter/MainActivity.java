422bc6e0e8889b86da6b3516bb23bf4adbc617ad
Biqi Sun <sun00201@algonquinlive.com>
2024-02-07 16:01:51 -0500

    Biqi Sun 041089070 Branch3 Commit1. Added Log messages at the entry/exit of onCreate() and convertCurrency methods. Level Info

--- app/src/main/java/com/college/converter/MainActivity.java
+++ app/src/main/java/com/college/converter/MainActivity.java
@@ -27,7 +27,6 @@ public class MainActivity extends AppCompatActivity {
     static private final Float CONVERSION_RATE = 0.80F;
     private static final String TAG = "MainActivity"; // Tag for log messages
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

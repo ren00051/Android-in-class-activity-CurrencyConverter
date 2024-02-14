dfb7160b46db4ad39bc9bf294c3f3eba44608642
Shanshan Ren <ren00051@algonquinlive.com>
2024-02-14 15:34:04 -0500

    Finished adding JUNIT test: Test2 in Branch 2.

--- app/src/androidTest/java/com/college/converter/MainActivityTest.java
+++ app/src/androidTest/java/com/college/converter/MainActivityTest.java
@@ -6,24 +6,18 @@ import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static androidx.test.espresso.matcher.ViewMatchers.withParent;
-import static androidx.test.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.Matchers.allOf;
 
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.ViewParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
 
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
-import org.hamcrest.Description;
-import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
